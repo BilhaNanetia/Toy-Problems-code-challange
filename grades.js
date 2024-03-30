b22e47b908a794ae67d042ec7aeb4cc8147f241f
Bilha <bilha.leposo@student.moringaschool.com>
2024-03-30 11:38:01 +0300

    corrects grades program

--- grades.js
+++ grades.js
@@ -1,27 +1,27 @@
- 
- //Prompt the user to enter the student's marks
- const marks = prompt("Please enter the student's marks(between 0 and 100):");
-
- //Validate user's input
- function calculateGrade(marks) {
-    if (marks < 0 || marks > 100) {
-        return "Invalid marks.Enter a value between 0 and 100.";//Input marks must be between 0 and 100
-    }
- }
+ function calculateGrade (marks) {
 
  //Assign grades based on student's marks
- let grade;
  if (marks >79) {
-    grade = "A";
+    return "A";
  } else if (marks >= 60){
-    grade = "B";
+    return "B";
  }else if (marks >= 50) {
-    grade = "C"; 
+    return "C"; 
  } else if (marks >= 40) {
-    grade = "D";
+    return "D";
  } else {
-    grade = "E";
+    return "E";
  }
+}
  
-  //Caculate and display the grade
- console.log (${grade});
+  //Get user input for student marks
+  let marks = parseFloat(prompt("Enter student's marks (between 0 and 100):"));
+  
+  //Validate the user's input
+  if (isNaN(marks) ||marks < 0 || marks > 100) {
+   return ('Invalid input.Marks are between 0 and 100');
+  } else {
+   //calculate and display the grade
+   const grade = calculateGrade(marks);
+   return Grade: ${grade};
+  }
